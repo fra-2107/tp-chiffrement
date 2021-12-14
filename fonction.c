8ed55679eee9f036d2b67523f078bd9a6c3288d2
fra-2107 <timchimienti@gmail.com>
2021-12-14 11:13:16 +0100

    commentaires sur main et fonctions

--- fonction.c
+++ fonction.c
@@ -1,58 +1,73 @@
 #include "cesar.h"
 
+//fonction de chiffrement : 
 char Chiffrement(char lettre, int cle)
 {
-  //Chaine qui correspond au mot chiffré
+  //Chaine qui correspond au caractère chiffré.
   char LettreChiffree = 0;
 
-  //Chiffrement du mot
+  //Chiffrement du mot :
   if(lettre>='a' && lettre <='z')
   {
+    //mise en majuscule des lettres minuscules (pour faciliter le traitement).
     lettre = toupper(lettre);
     LettreChiffree = lettre + cle;
+    //on retranche 26 pour rester dans l'alphabet de le pas arriver dans les caractères spéciaux.
     if (LettreChiffree >'Z')
       {
         LettreChiffree = LettreChiffree - ALPHA;
       }
+    //remise en minuscule des lettres minuscule.
     LettreChiffree = tolower(LettreChiffree);
   }
   else if (lettre>='A' && lettre <='Z')
   {
     LettreChiffree = lettre + cle;
+    //on retranche 26 pour rester dans l'alphabet de le pas arriver dans les caractères spéciaux.
     if (LettreChiffree >'Z')
       {
         LettreChiffree = LettreChiffree - ALPHA;
       }
   }
 
-  //Affichage du mot chiffré
+  //retour du caractère chiffré.
   return LettreChiffree;
 }
 
+//fonction de déchiffrement :
 char dechiffrement(char c, int k)
 {
+  //chaine qui correspond au caractère dechiffré.
   char dechcara=0;
+
+  //dechiffrement du mot:
   if(c>='a' && c <='z')
   {
+    //mise en majuscule des lettres minuscules (pour faciliter le traitement).
     c = toupper(c);
     dechcara = c - k;
+    //on ajoute 26 pour rester dans l'alphabet de le pas arriver dans les caractères spéciaux.
     if (dechcara <'A')
       {
         dechcara = dechcara + ALPHA;
       }
+      //remise en minuscule des lettres minuscule.
     dechcara = tolower(dechcara);
   }
   else if (c>='A' && c <='Z')
   {
     dechcara = c - k;
+    //on ajoute 26 pour rester dans l'alphabet de le pas arriver dans les caractères spéciaux.
     if (dechcara < 'A')
       {
         dechcara = dechcara + ALPHA;
       }
   }
+  //retour du caractère déchiffré.
   return dechcara;
 }
 
+//fonction de recherche de la lettre la plus présente dans le texte chiffré :
 int lettreMax(char *chaine)
 {
   int tab[ALPHA]={0};
@@ -61,6 +76,7 @@ int lettreMax(char *chaine)
   //creation d'un tableau avec 26 valeurs correspondants aux lettres de l'alphabet.
   for (int i=0; i<a; i++)
   {
+    //incrementation du rang correspondant au caractère.
     if (chaine[i]>= 'A' && chaine[i]<='Z')
     {
       tab[chaine[i]-'A']++;
@@ -86,11 +102,13 @@ int lettreMax(char *chaine)
 
 }
 
+//focntion de calcul de la clé de déchiffrement :
 int CalculerCle(char cara)
 {
   int vale= 'E'-'A';
   int cle;
   cle= (cara-'A') - vale;
+
   printf("La cle est : %d\n",cle);
   return cle;
 }
--- main.c
+++ main.c
@@ -2,50 +2,59 @@
 
 int main(int argc, char  *argv[])
 {
+  //mode "chiffrement" :
   if (strcmp(argv[1],"-c")==0)
   {
     char phrase[CHMAX];
     int cle;
     int i = 0;
-    //recuperation de la chaine et de la clé.
+
+    //récupération de la chaine à chiffrer.
     printf("Entrez une phrase ($+enter pour arreter): ");
     while ((phrase[i] = getchar()) != '$')
     {
       i++;
     }
-    printf("Entrez une cle : ");
-    scanf("%d", &cle);
 
     //allocation dynamique d'une chaine.
     char *phrase1 = NULL;
     phrase1 = malloc(strlen(phrase)*sizeof(char));
     strcpy(phrase1, phrase);
     phrase1[i] = '\0';
+    
+    //récupération de la clé de chiffrement.
+    printf("Entrez une cle : ");
+    scanf("%d", &cle);
 
     //chiffrement du texte.
     printf("Texte chiffre : ");
     for (i = 0; i < strlen(phrase1); i++)
     {
+      //chiffrement des lettres uniquement.
       if ((phrase1[i] >= 'A' && phrase1[i] <= 'Z' )|| (phrase1[i] >= 'a' && phrase1[i] <= 'z'))
       {
         printf("%c", Chiffrement(phrase1[i], cle));
       }
+      //réaffichage des caractères speciaux non chiffrés. 
       else
       {
         printf("%c", phrase[i]);
       }
     }
+
+    //liberation de la mémoire.
     free (phrase1);
     printf("\n");
   }
 
+  //mode "déchiffrement" :
   else if (strcmp(argv[1],"-d")==0)
   {
     char chaine[CHMAX];
     int i=0, cle=0;
 
-    //recuperation de la chaine.
-    printf("Veuillez entrer le texte chiffre ($+enter pour finir)\n");
+    //récupération de la chaine à déchiffrer.
+    printf("Veuillez entrer le texte chiffre ($+enter pour finir) : ");
     while ((chaine[i]=getchar())!= '$')
     {
       i++;
@@ -58,34 +67,38 @@ int main(int argc, char  *argv[])
     chaine1[i]='\0';
     printf("votre texte chiffre : %s\n", chaine1);
 
-    //recuperation de la clé
-    printf("veuillez entrer la cle :\n");
+    //recuperation de la clé de déchiffrement.
+    printf("veuillez entrer la cle : ");
     scanf("%d", &cle);
 
     //affichage de la chaine dechiffrée.
     printf("votre texte dechiffre : ");
     for (int i=0; i<strlen(chaine1); i++)
     {
+      //déchiffrement des lettres uniquement.
       if((chaine1[i] >= 'A' && chaine1[i] <= 'Z' )|| (chaine1[i] >= 'a' && chaine1[i] <= 'z'))
       {
         printf("%c", dechiffrement(chaine1[i], cle));
       }
+      //réaffichage des caractères spéciaux.
       else
       {
         printf("%c", chaine1[i]);
       }
     }
-    printf("\n");
-    free (chaine1);
 
+    //liberation de la mémoire.
+    free (chaine1);
+    printf("\n");
   }
 
+  //mode "automatique" :
   else if (strcmp(argv[1],"-a")==0)
   {
     char chaine [CHMAX];
     int i=0;
 
-    //recuperation de la chaine.
+    //récuperation de la chaine chiffrée.
     printf("Veuillez entrer le texte ($+enter pour finir)\n");
     while ((chaine[i]=getchar())!= '$')
     {
@@ -104,7 +117,7 @@ int main(int argc, char  *argv[])
 
     int cleAuto=0;
 
-    //recherche de la clé
+    //recherche de la clé de déchiffrement.
     cleAuto=CalculerCle(x);
 
     i=0;
@@ -115,17 +128,21 @@ int main(int argc, char  *argv[])
     printf("votre texte dechiffree : ");
     for (int i=0; i<strlen(chaine1); i++)
     {
+      //déchiffrement des lettres uniquement.
       if ((chaine1[i]>= 'A' && chaine1[i]<='Z')||(chaine1[i]>= 'a' && chaine1[i]<='z'))
       {
         printf("%c", dechiffrement(chaine1[i], cleAuto));
       }
+      //réaffichage des caractères spéciaux.
       else 
       {
         printf("%c", chaine1[i]);
       }
     }
-    printf("\n");
+
+    //libération de la mémoire.
     free (chaine1);
+    printf("\n");
   }
 
   return 0;
